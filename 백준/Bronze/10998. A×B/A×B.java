8feb65f4d05ff4d7e6cf7be81abd9c39786f8ead
차은영 <sts07129@naver.com>
2024-09-20 00:38:48 +0900

    [Bronze V] Title: A×B, Time: 164 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +6,8 @@ public class Main {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
+
+        sc.close();
 
         System.out.println(a * b);
     }
