902dca365c6e6b42bf53cad37cc181c6cc7101d8
차은영 <sts07129@naver.com>
2024-12-06 01:01:25 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 156 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -11,37 +11,33 @@ public class Main {
         
         //n개의 풍선
         int n = Integer.parseInt(br.readLine());
-        Deque<Integer> balloons = new ArrayDeque<>();
+        Deque<int[]> balloons = new ArrayDeque<>();
 
         //풍선 안에 적인 숫자
         String[] input = br.readLine().split(" ");
-        Deque<Integer> numbers = new ArrayDeque<>();
-
         for(int i = 0; i < n; i++){
-            balloons.offer(i + 1);
-            numbers.offer(Integer.parseInt(input[i]));
+            int num = Integer.parseInt(input[i]);
+            balloons.offer(new int[]{i + 1, num});
         }
-        
-        sb.append(balloons.poll()).append(" "); //첫번째 풍선 터뜨리기
-        int paperNum = numbers.poll();
+
         while(!balloons.isEmpty()){
-            if(paperNum > 0){
-                for(int i = 0; i < paperNum - 1; i++){
-                    balloons.offer(balloons.poll());
-                    numbers.offer(numbers.poll());
+            int[] ballon = balloons.poll();
+            int paperNum = ballon[1];
+            sb.append(ballon[0]).append(" ");
+
+            if(!balloons.isEmpty()){
+                if(paperNum > 0){
+                    for(int i = 0; i < paperNum - 1; i++){
+                        balloons.offer(balloons.pollFirst());
+                    }
+                }else{
+                    for(int i = 0; i < Math.abs(paperNum); i++){
+                        balloons.offerFirst(balloons.pollLast());
+                    }
                 }
-                sb.append(balloons.poll()).append(" ");
-                paperNum = numbers.poll();
-            }else{
-                paperNum *= -1;
-                for(int i = 0; i < paperNum; i++){
-                    balloons.offerFirst(balloons.pollLast());
-                    numbers.offerFirst(numbers.pollLast());
-                }
-                sb.append(balloons.poll()).append(" ");
-                paperNum = numbers.poll();
             }
         }
+
         System.out.println(sb);
 
     }
