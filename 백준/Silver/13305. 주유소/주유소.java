5bb55949bc4d1352410d9f7eb43de8734c9ebebb
차은영 <sts07129@naver.com>
2025-02-17 14:59:43 +0900

    [Silver III] Title: 주유소, Time: 312 ms, Memory: 34128 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,37 +9,27 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         
         //도로의 길이, 리터당 가격 입력받기
-        int[] road = new int[N - 1];
-        int[] city = new int[N];
+        int[] distances = new int[N - 1];
+        int[] prices = new int[N];
         String[] roadInput = br.readLine().split(" ");
         String[] cityInput = br.readLine().split(" ");
         for(int i = 0; i < N - 1; i++){
-            road[i] = Integer.parseInt(roadInput[i]);
+            distances[i] = Integer.parseInt(roadInput[i]);
         }
         for(int i = 0; i < N; i++){
-            city[i] = Integer.parseInt(cityInput[i]);
+            prices[i] = Integer.parseInt(cityInput[i]);
         }
         
-        //가격 산정
-        int oilCost = road[0] * city[0]; //첫 도시에서 다음도시로 이동하는 가격
+        int minCost = 0; //최소 비용
+        int minPrice = prices[0]; //처음 주유소 가격
 
-        //도시 중 기름값이 가장 작은 도시 찾기 (첫번째, 마지막 도시 제외)
-        int minCity = Integer.MAX_VALUE;
-        for(int i = 1; i < N - 1; i++){
-            if(city[i] < minCity){
-                minCity = city[i];
+        for(int i = 0; i < N - 1; i++){
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
             }
+            minCost += minPrice * distances[i];
         }
 
-        //남은 거리를 기름값이 가장 작은 도시에서 충전
-        int remainRoad = 0;
-        for(int i = 1; i < N - 1; i++){
-            remainRoad += road[i];
-        }
-
-        oilCost += remainRoad * minCity;
-
-        System.out.println(oilCost);
-
+        System.out.println(minCost);
     }
 }
