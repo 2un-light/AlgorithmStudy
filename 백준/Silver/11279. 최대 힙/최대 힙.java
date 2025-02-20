a81bc2deaf3b00cca90cd3a6c74804a55edb76bd
차은영 <sts07129@naver.com>
2025-02-21 00:29:20 +0900

    [Silver II] Title: 최대 힙, Time: 284 ms, Memory: 27696 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,28 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+class MaxHeap {
+    private int[] heap;
+    private int size;
+
+    public MaxHeap(int capacity) {
+        heap = new int[capacity + 1];
+        size = 0;
+    }
+
+    public void insert(int value) {
+        heap[++size] = value;
+        int current = size;
+
+        //상향식 heapify
+        while(current > 1 && heap[current] > heap[current / 2]) {
+            swap(current, current / 2);
+            current /= 2;
+        }
+
+    }
+
+    public int extractMax() {
+        if(size == 0) return 0;
+
+        int max = heap[1];
+        heap[1] = heap[size--];
+        heapify(1);
+        return max;
+    }
+
+    private void heapify(int index) {
+        int largest = index;
+        int left = 2 * index, right = 2 * index + 1;
+
+        if(left <= size && heap[left] > heap[largest]) largest = left;
+        if(right <= size && heap[right] > heap[largest]) largest = right;
+
+        if(largest != index) {
+            swap(index, largest);
+            heapify(largest);
+        }
+    }
+
+    private void swap(int a, int b) {
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        MaxHeap mh = new MaxHeap(N);
 
         for(int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0) {
-                if(maxHeap.isEmpty()){
-                    System.out.println(0);
-                }else {
-                    System.out.println(maxHeap.poll());
-                }
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                bw.write(mh.extractMax() + "\n");
             }else {
-                maxHeap.offer(num);
+                mh.insert(x);
             }
         }
+
+        bw.flush();
+        br.close();
+        bw.close();
 
     }
 }
