fde129422857a0af7a58895ca73b1a39a51a37d8
차은영 <sts07129@naver.com>
2025-08-10 14:41:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 196 ms, Memory: 19256 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int n, m, v;
+    public static ArrayList<Integer>[] graph;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        //간선 입력
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for(int i = 1; i <= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[n + 1];
+        dfs(v);
+        sb.append("\n");
+
+        visited = new boolean[n + 1];
+        bfs(v);
+
+        System.out.println(sb);
+
+
+    }
+
+    public static void dfs(int node) {
+        visited[node] = true;
+        sb.append(node).append(" ");
+        for(int next : graph[node]) {
+            if(!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.add(start);
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            sb.append(node).append(" ");
+
+            for(int next : graph[node]) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
