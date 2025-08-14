1521d630b67bd5591a7a39ded2592b3b913a8a00
차은영 <sts07129@naver.com>
2025-08-14 20:34:20 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10436 ms, Memory: 304980 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static ArrayList<ArrayList<Integer>> graph;
-    static boolean[] visited;
     static int[] hackCounts;
 
     public static void main(String[] args) throws IOException {
@@ -38,16 +39,12 @@ public class Main {
         
         //각 컴퓨터를 시작점으로 DFS 탐색하기
         for(int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            dfs(i, i);
-        }
-
-        //최대 해킹 가능한 컴퓨터 수 찾기
-        for(int i = 1; i <= N; i++) {
+           hackCounts[i] =  bfs(i);
             if(hackCounts[i] > maxHack) {
                 maxHack = hackCounts[i];
             }
         }
+
 
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= N; i++) {
@@ -57,20 +54,31 @@ public class Main {
         }
 
         System.out.println(sb);
-        
-    }
-
-    public static void dfs(int startNode, int currentNode) {
-        visited[currentNode] = true;
-        hackCounts[startNode]++;
-
-        for(int next : graph.get(currentNode)) {
-            if(!visited[next]) {
-                dfs(startNode, next);
-            }
-        }
 
     }
 
+   public static int bfs(int startNode) {
+       Queue<Integer> queue = new LinkedList<>();
+       boolean[] visited = new boolean[N + 1];
+
+       queue.offer(startNode);
+       visited[startNode] = true;
+       int cnt = 1;
+
+       while (!queue.isEmpty()) {
+           int node = queue.poll();
+
+           for (int next : graph.get(node)) {
+               if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                    cnt++;
+               }
+           }
+
+       }
+
+       return cnt;
+   }
 
 }
