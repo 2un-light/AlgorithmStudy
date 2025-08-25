65ec147e61014a61e013236a16858d2031845397
차은영 <sts07129@naver.com>
2025-08-25 15:12:10 +0900

    [Silver IV] Title: 큐, Time: 168 ms, Memory: 20564 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        while(N-- > 0) {
+            String[] parts = br.readLine().split(" ");
+            String cmd = parts[0];
+            switch (cmd) {
+                case "push":
+                    queue.offer(Integer.parseInt(parts[1]));
+                    break;
+                case "pop":
+                    sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : ((LinkedList<Integer>)queue).peekLast()).append("\n");
+                    break;
+
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
