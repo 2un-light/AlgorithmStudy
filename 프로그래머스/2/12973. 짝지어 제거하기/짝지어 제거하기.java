f86d9f182ea5cbe64cdac29f247215730b44be90
차은영 <sts07129@naver.com>
2025-10-16 18:12:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.93 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop(); //연속된 문자 제거
+            }else {
+                stack.push(c); //새 문자 추가
+            }
+        }
+        
+        return stack.isEmpty() ? 1 : 0;
+        
+    }
+}
