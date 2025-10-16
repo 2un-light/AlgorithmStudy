b840aad582365c3f600ba2ee26ba2f082559f017
차은영 <sts07129@naver.com>
2025-10-16 22:51:24 +0900

    [level 2] Title: 귤 고르기, Time: 87.83 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         
-        Map<Integer, Integer> tangerineMap = new HashMap<>(); //<귤 사이즈, 해당 사이즈 귤의 갯수>
         for(int size : tangerine) {
-            tangerineMap.put(size, tangerineMap.getOrDefault(size, 0) + 1);
+            map.put(size, map.getOrDefault(size, 0) + 1);
         }
         
-        List<Integer> counts = new ArrayList<>(tangerineMap.values());
+        List<Integer> counts = new ArrayList(map.values());
         Collections.sort(counts, Collections.reverseOrder());
         
         int sum = 0;
-        int kinds = 0;
+        int kind = 0;
         
         for(int count : counts) {
             sum += count;
-            kinds++;
+            kind++;
             if(sum >= k) break;
         }
         
-        return kinds;
+        return kind;
+    
         
     }
 }
