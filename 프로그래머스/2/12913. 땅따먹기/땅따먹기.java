838f1db23cec3e33cf257ae8ccc75f08f64b9a04
차은영 <sts07129@naver.com>
2025-10-17 19:43:49 +0900

    [level 2] Title: 땅따먹기, Time: 22.04 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,24 @@
+class Solution {
+    int solution(int[][] land) {
+        int n = land.length;
+        
+        for(int i = 1; i < n; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+        
+        int[] lastRow = land[n - 1];
+        
+        int answer = 0;
+        for(int num : lastRow) {
+            if(answer < num) {
+                answer = num;
+            }
+        }
+        
+        return answer;
+        
+    }
+}
