bbfcd140f2d8f88287624b60de8deecf5ce37f54
차은영 <sts07129@naver.com>
2025-10-31 04:11:04 +0900

    [level 1] Title: 과일 장수, Time: 334.27 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,24 +4,13 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         
-        //역순 정렬
-        Arrays.sort(score);
+        Integer[] arr = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Collections.reverseOrder());
         
-        for(int i = 0; i < score.length / 2; i++) {
-            int temp = score[i];
-            score[i] = score[score.length - 1 - i];
-            score[score.length - 1 - i] = temp;
+        for(int i =  m - 1; i < arr.length; i += m) {
+            answer += arr[i] * m;
         }
         
-        int box = score.length / m;
-        
-        int index = m - 1;
-        while(box > 0) {
-            int price = score[index] * m;
-            answer += price;
-            index += m;
-            box--;
-        }
         
         return answer;
     }
