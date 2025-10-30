a0975afec8db5f249a260d42f16b4c3f0252de27
차은영 <sts07129@naver.com>
2025-10-31 02:59:51 +0900

    [level 1] Title: 기사단원의 무기, Time: 75.44 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for(int i = 1; i <= number; i++) {
+            int cnt = getDivisorCnt(i);
+            
+            if(cnt > limit) {
+                answer += power;
+            }else {
+                answer += cnt;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    private int getDivisorCnt(int num) {
+        int cnt = 0;
+        for(int i = 1; i * i <= num; i++) {
+            if(num % i == 0) {
+                cnt++;
+                if(i * i != num) cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+}
