4edd637a792d2c2c1bd998e9687e94f500861e08
차은영 <sts07129@naver.com>
2026-02-25 19:38:39 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 120176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,74 +6,70 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, m;
-    public static int[][] box;
 
-    public static boolean[][] visited;
-
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
-
-    public static int days = 0;
-
+    static int N, M;
+    static int[][] box;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
 
-        box = new int[n][m];
-        visited = new boolean[n][m];
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
+        box = new int[N][M];
         Queue<int[]> queue = new LinkedList<>();
 
-        for(int i = 0; i < n; i++) {
+        //박스 입력받기
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
+            for(int j = 0; j < M; j++) {
                 box[i][j] = Integer.parseInt(st.nextToken());
-                if(box[i][j] == 1) {
-                    queue.offer(new int[]{i, j});
-                }
+                if(box[i][j] == 1) queue.offer(new int[]{i, j});
             }
         }
 
-        int days = -1;
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int s = 0; s < size; s++) {
-                int[] loc = queue.poll();
-                int cx = loc[0];
-                int cy = loc[1];
+        bfs(queue);
 
-                for(int dir = 0; dir < 4; dir++) {
-                    int nx = cx + dx[dir];
-                    int ny = cy + dy[dir];
+        int result = 0;
 
-                    if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                        if(box[nx][ny] == 0) {
-                            box[nx][ny] = 1;
-                            queue.offer(new int[]{nx, ny});
-                        }
-                    }
-                }
-            }
-
-            days++;
-        }
-
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
                 if(box[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
+                result = Math.max(result, box[i][j]);
             }
         }
 
-        System.out.println(days);
+        System.out.println(result - 1);
+
+    }
+
+    public static void bfs(Queue<int[]> queue) {
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for(int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(box[nx][ny] != 0) continue;
+
+
+                box[nx][ny] = box[cx][cy] + 1;
+                queue.offer(new int[]{nx, ny});
+
+            }
+        }
 
     }
 
