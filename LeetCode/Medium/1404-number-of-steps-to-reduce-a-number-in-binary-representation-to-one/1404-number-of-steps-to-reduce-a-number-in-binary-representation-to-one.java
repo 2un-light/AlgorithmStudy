f168c6e259d87226515ae5e3f37dff18dda9b609
차은영 <sts07129@naver.com>
2026-02-26 10:57:01 +0900

    1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one

---
+++ LeetCode/Medium/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSteps(String s) {
+        int count = 0;
+        int carry = 0;
+
+        for(int i = s.length() - 1; i > 0; i--) {
+            int bit = (s.charAt(i) - '0') + carry;
+
+            if(bit % 2 == 1) {
+                count += 2;
+                carry = 1;
+            }else {
+                count += 1;
+            }
+        }
+
+        count += carry;
+        return count;
+
+    }
+}
