6c80f209d47159e14ede383ce1552c1f372da80c
차은영 <sts07129@naver.com>
2026-02-28 13:55:10 +0900

    1680-concatenation-of-consecutive-binary-numbers

---
+++ LeetCode/Medium/1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int modulo = 1000000007;
+
+        for(int i = 1; i <= n; i++) {
+            String binary = Integer.toBinaryString(i);
+            for(char c : binary.toCharArray()) {
+                result = (result * 2 + (c - '0')) % modulo;
+            }
+        }
+
+        return (int) result;
+    }
+}
