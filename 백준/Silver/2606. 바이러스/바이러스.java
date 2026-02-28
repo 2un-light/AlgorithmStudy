372a5ef85eea1d9fac1b05992e5f047bbd301dc2
차은영 <sts07129@naver.com>
2026-02-28 18:20:08 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,64 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int N, M;
     static List<Integer>[] graph;
     static boolean[] visited;
-
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N + 1];
 
         graph = new ArrayList[N + 1];
         for(int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        visited = new boolean[N + 1];
 
         for(int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int com1 = Integer.parseInt(st.nextToken());
+            int com2 = Integer.parseInt(st.nextToken());
 
-            graph[x].add(y);
-            graph[y].add(x);
-
+            graph[com1].add(com2);
+            graph[com2].add(com1);
         }
 
-        System.out.println(bfs(1));
-
+        dfs(1);
+        System.out.println(count);
 
     }
 
-    private static int bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+    public static void dfs(int start) {
         visited[start] = true;
 
-        int count = 0;
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            for(int next : graph[cur]) {
-                if(visited[next]) continue;
-
-                visited[next] = true;
+        for(int next : graph[start]) {
+            if(!visited[next]) {
                 count++;
-                queue.offer(next);
+                visited[next] = true;
+                dfs(next);
             }
         }
-
-        return count;
-
-
     }
-
 }
