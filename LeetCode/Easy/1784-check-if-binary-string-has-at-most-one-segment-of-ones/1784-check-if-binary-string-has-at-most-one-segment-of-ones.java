e83c806aa507ae4681b2cd7de0298c43b187118d
차은영 <sts07129@naver.com>
2026-03-06 11:12:07 +0900

    1784-check-if-binary-string-has-at-most-one-segment-of-ones

--- LeetCode/Easy/1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ LeetCode/Easy/1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
         
-        int part = 0; //1부터 시작, 1의 인덱스
+        int lastOneIndex = 0; //1부터 시작, 1의 인덱스
         for(int i = 1; i < s.length(); i++) {
             char current = s.charAt(i);
             if(current == '1') {
-                if(part + 1 == i) {
-                    part = i;
+                if(lastOneIndex + 1 == i) {
+                    lastOneIndex = i;
                 }else {
                     return false;
                 }
