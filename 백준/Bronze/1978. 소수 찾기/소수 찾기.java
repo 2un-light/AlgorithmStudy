07deaaff3bd5cb3eeb9b437bdd5097d9eaf1fada
차은영 <sts07129@naver.com>
2026-03-31 23:00:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -8,31 +8,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int primeCount = 0;
-
-        while(st.hasMoreTokens()) {
+        for(int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(isPrime(num)){
-                primeCount++;
-            }
+            if(isPrime(num)) count++;
         }
 
-        System.out.println(primeCount);
+        System.out.println(count);
 
     }
 
-    public static boolean isPrime(int num){
-        if(num <= 1){
-            return false;
+    private static boolean isPrime(int num) {
+        if(num < 2) return false;
+        if(num == 2) return true;
+        if(num % 2 == 0) return false;
+
+        for(int i = 3; i * i <= num; i += 2) {
+            if(num % i == 0) return false;
         }
 
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
         return true;
     }
 }
