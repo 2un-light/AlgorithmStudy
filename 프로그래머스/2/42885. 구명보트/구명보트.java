173d963de888fb9ac5461e6c87aab4b0b4a865ee
차은영 <sts07129@naver.com>
2026-05-12 20:53:15 +0900

    [level 2] Title: 구명보트, Time: 5.52 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people); //[50, 50, 70, 80]
+        Arrays.sort(people);
+        
         int left = 0;
         int right = people.length - 1;
+        
         int answer = 0;
         
         while(left <= right) {
+            
+            //같이 탈 수 있으면
             if(people[left] + people[right] <= limit) {
                 left++;
             }
+            
+            //무거운 사람은 항상 태우기
             right--;
+            
             answer++;
         }
         
