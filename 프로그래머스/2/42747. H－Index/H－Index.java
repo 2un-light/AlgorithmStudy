a94f2da6724922fc3a9d89e5252462c851e6e759
차은영 <sts07129@naver.com>
2026-05-28 16:56:20 +0900

    [level 2] Title: H-Index, Time: 0.66 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,27 +1,21 @@
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) {
-        int[] citations = {3,0,6,1,5};
-
-        System.out.println(solution(citations));
-
-    }
-
-    public static int solution(int[] collections) {
-        int answer = 0;
-        int n = collections.length;
-
-        Arrays.sort(collections);
-
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        
         for(int i = 0; i < n; i++) {
             int h = n - i;
-            if(collections[i] >= h){
-                answer = h;
-                break;
+            
+            if(citations[i] >= h) {
+                return h;
             }
         }
-
-        return answer;
+        
+        return 0;
     }
 }
